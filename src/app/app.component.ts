e5821a4bef97523c289145003190998946527b81
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Air.local>
2022-05-03 18:41:11 -0700

    add library pipes and custom pipe"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   'name': string;
+  'date': string;
+  'amount': number;
+  'height': number;
+  'miles': number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
 
   onNameChange(event:any){
     this.name = event.target.value
   }
+
+  onDateChange(event: any){
+    this.date = event.target.value
+  }
+
+  
+  onAmountChange(event: any){
+    this.amount = parseFloat(event.target.value)
+  }
+
+  onHeightChange(event: any){
+    this.height = parseFloat(event.target.value)
+  }
+
+  onMilesChange(event: any){
+    this.miles = parseFloat(event.target.value)
+  }
 }
+
